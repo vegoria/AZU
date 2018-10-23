0490c272f39f519775a0a4a71d1e9f43a696d248
vegoria <vegoria@gmail.com>
2018-10-23 14:04:36 +0200

    path fix

--- lab2/SKaletaKWanat/src/java/lab02/GenericResource.java
+++ lab2/SKaletaKWanat/src/java/lab02/GenericResource.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab02;
 
 import java.sql.Timestamp;
@@ -11,27 +6,22 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import javax.ejb.EJB;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.CookieParam;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Cookie;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 
-@Path("/my_app")
+@Path("")
 public class GenericResource {
     @EJB
     private IUserList userListContainer;
