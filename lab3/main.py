f2faa9cb41c3127a5987569f63348defba3104a0
vegoria <vegoria@gmail.com>
2018-10-27 20:52:15 +0200

    xsd schema for klasa

--- lab3/main.py
+++ lab3/main.py
@@ -1,6 +1,6 @@
 import xmlschema
-import generateDS
 from enum import Enum
+from xml.etree import ElementTree
 
 class TypOsoby(Enum):
     NAUCZYCIEL = 1
@@ -13,26 +13,58 @@ class Osoba():
         self.nazwisko = None
         self.rokUrodzenia = None
 
+    def toDict(self):
+        dict = {
+            "typOsoby": self.typOsoby,
+            "imie": self.imie,
+            "nazwisko": self.nazwisko,
+            "rokUrodzenia": self.rokUrodzenia
+        }
+        return dict
+
 class Klasa():
     def __init__(self):
         self.numer = None
         self.litera = None
         self.wychowawca = None
-        self.uczniowie = {}
+        self.uczniowie = []
+
+    def toDict(self):
+        dict = {
+            "klasa": {
+                "numer": self.numer,
+                "litera": self.litera,
+                "wychowawca": self.wychowawca.toDict(),
+                "uczniowie": [ucz.toDict()for ucz in self.uczniowie]}
+        }
+        return dict
+
+    def makeXML(self):
+        pass
 
 def saveToXml(klasa):
-    pass
+    data = klasa.makeXML() #TODO zaimplementowac makeXML
+    file = open('output.xml', 'w')
+    file.write(data)
+    file.close()
 
 def saveFromXml():
-    pass
-
+    print("Podaj sciezke do pliku xml:")
+    xmlPath = input()
+    print("Podaj sciezke do schematu xsd")
+    xsdPath = input()
+    parsedXml = ElementTree.parse(xmlPath)
+    mySchema = xmlschema.XMLSchema(xsdPath)
+    root = parsedXml.getroot()
+    # klasy = mySchema.elements['klasa'].decode(root[0]))
+    #  #TODO finish parsing
 
 klasa = None
 option = 1
 while option !='3':
     print('Co chcesz zrobic?')
     print('1. Stworzyc xml z klasy')
-    print('1. Stworzyc obiekt z pliku xml')
+    print('2. Stworzyc obiekt z pliku xml')
     print('3. Walidacja pliku XML')
     print('4. Wyjscie')
     option = input()
